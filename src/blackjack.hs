24023d4a2f5d7b693642ec9e50ef8598b4ca4eaf
Danillo Souza <danillosouza1704@gmail.com>
2022-02-07 20:29:25 -0300

    Adicionado ToDo e criação de baralho

---
+++ src/blackjack.hs
@@ -0,0 +1,16 @@
+module Blackjack
+
+-- Criando baralho
+chips = 1000
+
+suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+values = concat [['1'..'9'], ['A', 'J', 'Q', 'K']]
+
+deck = [(x, y) | x <- suits, y <- values]
+
+-- ToDo: Remover carta aleatoria e atualizar o valor do baralho 
+-- ToDo: Valor do baralho deve ser reiniciado a cada partida
+-- ToDo: Adicionar possibilidade do jogador apostar valor do pote e atualizar o mesmo
+-- ToDo: Jogador pode apostar normal, dobrar valor da aposta ou passar
+-- ToDo: Jogador pode escerrar jogo no final da partida ou assim que falir
+-- ToDo (Opcional): Valor de aposta pode ser variavel
