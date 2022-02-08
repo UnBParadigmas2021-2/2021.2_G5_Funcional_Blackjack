4693185f363f7d5658a2abf67721802c52ef3026
Danillo Gonçalves Souza <danillosouza1704@gmail.com>
2022-02-07 21:05:51 -0300

    Corrigido Typo

--- src/blackjack.hs
+++ src/blackjack.hs
@@ -12,6 +12,6 @@ deck = [(x, y) | x <- suits, y <- values]
 -- ToDo: Valor do baralho deve ser reiniciado a cada partida
 -- ToDo: Adicionar possibilidade do jogador apostar valor do pote e atualizar o mesmo
 -- ToDo: Jogador pode apostar normal, dobrar valor da aposta ou passar
--- ToDo: Jogador pode escerrar jogo no final da partida ou assim que falir
+-- ToDo: Jogador pode encerrar jogo no final da partida ou assim que falir
 -- ToDo (Opcional): Valor de aposta pode ser variavel
 -- ToDo (Opcional): Adicionar ASSCI Art para quando o jogo encerrar
