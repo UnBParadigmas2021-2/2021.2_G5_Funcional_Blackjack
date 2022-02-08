c61a9e09ee0900e59bf9cf3fa05521ff9d10f9f0
Danillo Gonçalves Souza <danillosouza1704@gmail.com>
2022-02-08 19:18:24 -0300

    Update blackjack.hs

--- src/blackjack.hs
+++ src/blackjack.hs
@@ -9,9 +9,11 @@ values = concat [['1'..'9'], ['A', 'J', 'Q', 'K']]
 deck = [(x, y) | x <- suits, y <- values]
 
 -- ToDo: Remover carta aleatoria e atualizar o valor do baralho 
+-- ToDo: Comparar valores das cartas do jogador com as carta da mesa
 -- ToDo: Valor do baralho deve ser reiniciado a cada partida
 -- ToDo: Adicionar possibilidade do jogador apostar valor do pote e atualizar o mesmo
 -- ToDo: Jogador pode apostar normal, dobrar valor da aposta ou passar
 -- ToDo: Jogador pode encerrar jogo no final da partida ou assim que falir
 -- ToDo (Opcional): Valor de aposta pode ser variavel
 -- ToDo (Opcional): Adicionar ASSCI Art para quando o jogo encerrar
+-- ToDo (Opcional): Fazer front com Haskel Gloss 
