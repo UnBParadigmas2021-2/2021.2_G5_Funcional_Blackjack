d0df43afea9a1311e0658eb97474c1749de5502f
Danillo Souza <danillosouza1704@gmail.com>
2022-02-11 19:11:58 -0300

    Adicionado função de embaralhar deck e comparando cartas
    Co-authored-by: CarlosFiuza <carlosfiuza1234@gmail.com>

--- src/menu.hs
+++ src/menu.hs
@@ -1,10 +1,12 @@
+-- import Blackjack
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+import Blackjack
+import Control.Monad
+import Data.Char
+import Data.Typeable
+import System.Exit
 import System.IO
 import System.Random
-import Control.Monad
-import Blackjack
-import Data.Typeable
-import Data.Char
-import System.Exit
 
 
 main :: IO ()
@@ -24,6 +26,7 @@ mainMenu money = do
         "1" -> startGameMenu money
         "2" -> putStrLn "O jogo serah finalizado...\n"
 
+startGameMenu :: Int -> IO ()
 startGameMenu money = do
   putStrLn "Escolha sua acao: "
   putStrLn "1 - Apostar 10"
@@ -32,102 +35,105 @@ startGameMenu money = do
   putStrLn "4 - Apostar 250"
   putStrLn "5 - Apostar 500"
   putStrLn "6 - Voltar para o menu principal\n"
+
   option <- getLine
+  deckShuffled <- shuffle deck
+  let playerHand = head deckShuffled : [deckShuffled !! 1]
+  let dealerHand = [deckShuffled !! 2]
+  let counter = 3
+  let _deckShuffled = drop counter deckShuffled
+
   case option of
     "1" -> do
       putStrLn $ "Apostando: " ++ show 10
-      let hand = initialHand (mkStdGen 2021)
-          dealerHand = initialHand (mkStdGen 1999)
-      inGameMenu 10 (money -10) hand dealerHand
+      inGameMenu 10 (money -10) playerHand dealerHand _deckShuffled counter
     "2" -> do
       putStrLn $ "Apostando: " ++ show 50
-      let hand = initialHand (mkStdGen 2021)
-          dealerHand = initialHand (mkStdGen 1999)
-      inGameMenu 50 (money - 50) hand dealerHand
+      inGameMenu 50 (money - 50) playerHand dealerHand _deckShuffled counter
     "3" -> do
       putStrLn $ "Apostando: " ++ show 100
-      let hand = initialHand (mkStdGen 2021)
-          dealerHand = initialHand (mkStdGen 1999)
-      inGameMenu 100 (money - 100) hand dealerHand
+      inGameMenu 100 (money - 100) playerHand dealerHand _deckShuffled counter
     "4" -> do
       putStrLn $ "Apostando: " ++ show 250
-      let hand = initialHand (mkStdGen 2021)
-          dealerHand = initialHand (mkStdGen 1999)
-      inGameMenu 250 (money - 250) hand dealerHand
+      inGameMenu 250 (money - 250) playerHand dealerHand _deckShuffled counter
     "5" -> do
       putStrLn $ "Apostando: " ++ show 500
-      let hand = initialHand (mkStdGen 2021)
-          dealerHand = initialHand (mkStdGen 1999)
-      inGameMenu 500 (money - 500) hand dealerHand
+      inGameMenu 500 (money - 500) playerHand dealerHand _deckShuffled counter
     "6" -> do
       putStrLn "Voltando para o menu...\n"
       mainMenu money
 
-inGameMenu :: Int -> Int -> [([Char], Char)] -> [([Char], Char)] -> IO ()
-inGameMenu bet totalMoney hand dealerHand = do
-  putStrLn $ "sua mão " ++ show hand
+inGameMenu :: Int -> Int -> [([Char], Char)] -> [([Char], Char)] -> [([Char], Char)] -> Int -> IO ()
+inGameMenu bet totalMoney playerHand dealerHand deckShuffled counter = do
+  putStrLn $ "sua mão " ++ show playerHand
   putStrLn $ "mão do dealer " ++ show dealerHand
-  compareHandValues hand dealerHand bet totalMoney
+
+  compareHandValues playerHand dealerHand bet totalMoney
+
   putStrLn "Escolha sua acao: "
   putStrLn "1 - Dobrar aposta"
   putStrLn "2 - Comprar carta"
   putStrLn "3 - Fechar mao\n"
   option <- getLine
+
   case option of
     "1" -> do
       putStrLn $ "Dobrando aposta... Valor atual: " ++ show (bet * 2)
-      inGameMenu (bet * 2) (totalMoney - bet) hand dealerHand
+      let _playerHand = (deckShuffled !! (counter + 1)) : playerHand
+      let _deckShuffled = drop (counter + 1) deckShuffled
+
+      inGameMenu (bet * 2) (totalMoney - bet) _playerHand dealerHand _deckShuffled (counter + 1)
     "2" -> do
       putStrLn $ "Valor Total: " ++ show totalMoney
-      inGameMenu bet totalMoney hand dealerHand
+
+      let _playerHand = (deckShuffled !! (counter + 1)) : playerHand
+      let _deckShuffled = drop (counter + 1) deckShuffled
+
+      inGameMenu bet totalMoney _playerHand dealerHand _deckShuffled (counter + 1)
     "3" -> do
       putStrLn "Fechando mao...\n"
+
+      let _dealerHand = (deckShuffled !! (counter + 1)) : dealerHand
+      let _deckShuffled = drop (counter + 1) deckShuffled
+
+      compareHandValues playerHand _dealerHand bet totalMoney
+
       mainMenu totalMoney
-
-
-initialHand :: StdGen -> [([Char], Char)]
-initialHand genOne = do
-    let (randOne, newGen) = randomR (1,52) genOne :: (Int, StdGen)
-        (randTwo, anotherGen) = randomR (1,52) newGen :: (Int, StdGen)
-        hand = [deck !! randOne] ++ [deck !! randTwo]
-    --putStrLn $ "Total da mão " ++ show((randOne `mod` 13) + (randTwo `mod` 13) + 2 ) ++ " " ++ show hand
-    return hand !! 0
-    -- putStrLn $ "Total da mão " ++ show deck
 
 compareHandValues :: [([Char], Char)] -> [([Char], Char)] -> Int -> Int -> IO ()
 compareHandValues hand dealerHand bet totalMoney = do
-    let handValue = getHandValue hand
-        handValueDealer = getHandValue dealerHand
-    putStrLn $ "Valor da sua mão " ++ show handValue
-    if handValue > 21 then do
+  let handValue = getHandValue hand
+      handValueDealer = getHandValue dealerHand
+  putStrLn $ "Valor da sua mão " ++ show handValue
+  if handValue > 21
+    then do
       putStrLn "Dealer venceu\n"
       mainMenu totalMoney
       exitSuccess
-    else if handValueDealer > 21 then do
-      putStrLn "Você venceu\n"
-      putStrLn $ "Seu dinheiro " ++ show (totalMoney + (2*bet)) ++ "\n"
-      mainMenu (totalMoney + (2*bet))
-      exitSuccess
-    else do
-      putStrLn ""
-  
+    else
+      if handValueDealer > 21
+        then do
+          putStrLn "Você venceu\n"
+          putStrLn $ "Seu dinheiro " ++ show (totalMoney + (2 * bet)) ++ "\n"
+          mainMenu (totalMoney + (2 * bet))
+          exitSuccess
+        else do
+          putStrLn ""
 
-getHandValue :: [([Char], Char)] -> Int
-getHandValue [] = 0
-getHandValue (h:t) = assignValueToCard h + getHandValue t 
+getHandValue :: [(a, Char)] -> Int
+getHandValue = foldr ((+) . assignValueToCard) 0
 
-assignValueToCard :: ([Char], Char) -> Int
+assignValueToCard :: (a, Char) -> Int
 assignValueToCard x = do
-    let bool = isFamilyCard (snd x)
-    case bool of
-        True -> 10
-        False -> digitToInt (snd x)
+  let bool = isFamilyCard (snd x)
+  case bool of
+    True -> 10
+    False -> digitToInt (snd x)
 
 isFamilyCard :: Char -> Bool
-isFamilyCard x  | x == 'A' = True
-                | x == 'J' = True
-                | x == 'Q' = True
-                | x == 'K' = True
-                | otherwise = False
-
-
+isFamilyCard x
+  | x == 'A' = True
+  | x == 'J' = True
+  | x == 'Q' = True
+  | x == 'K' = True
+  | otherwise = False
